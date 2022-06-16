f8d7a73635ac29f1892c8f7beb8cafa6afbf12f1
greg smith <gregalexsmith@gmail.com>
2022-06-16 01:58:57 -0400

    build(repo): update release config

--- release.config.js
+++ release.config.js
@@ -3,13 +3,13 @@ module.exports = {
   branches: ['main', 'dev'],
   plugins: [
     '@semantic-release/commit-analyzer',
-    //   '@semantic-release/release-notes-generator',
-    //   [
-    //     '@semantic-release/changelog',
-    //     {
-    //       changelogFile: 'CHANGELOG.md'
-    //     }
-    //   ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
     '@semantic-release/npm',
     '@semantic-release/github',
     [
