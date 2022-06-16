6873a9d8a57a82c91dde636f61e00e0e5766598d
greg smith <gregalexsmith@gmail.com>
2022-06-16 01:27:30 -0400

    build(repo): update commitlint rules

--- packages/components/release.config.js
+++ packages/components/release.config.js
@@ -1,4 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: '../../release.config.js'
+  extends: '../../release.config.js',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [{ scope: 'components', release: true }],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ]
+  ]
 };
