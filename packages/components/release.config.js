e24ce46f71531d8f7e991f94e2a568077dd5f796
greg smith <gregalexsmith@gmail.com>
2022-06-16 01:03:29 -0400

    build: fix release.config

--- packages/components/release.config.js
+++ packages/components/release.config.js
@@ -1,32 +1,24 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  release: {
-    branches: [
-      'main',
+  branches: ['main', 'dev'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    //   '@semantic-release/release-notes-generator',
+    //   [
+    //     '@semantic-release/changelog',
+    //     {
+    //       changelogFile: 'CHANGELOG.md'
+    //     }
+    //   ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
       {
-        name: 'beta',
-        prerelease: true
+        assets: ['CHANGELOG.md', 'dist/**'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
-    ],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      //   '@semantic-release/release-notes-generator',
-      //   [
-      //     '@semantic-release/changelog',
-      //     {
-      //       changelogFile: 'CHANGELOG.md'
-      //     }
-      //   ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'dist/**'],
-          message:
-            'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ]
     ]
-  }
+  ]
 };
