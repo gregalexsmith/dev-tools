2eaecf8f8c2ca9c59925e2bc03d366107c4ef1cc
greg smith <gregalexsmith@gmail.com>
2022-06-18 18:46:52 -0400

    fix(scripts): ensure build only runs when needed

--- packages/scripts/src/npm-publish/index.ts
+++ packages/scripts/src/npm-publish/index.ts
@@ -5,24 +5,28 @@ import paths from '../paths';
 
 const main = async () => {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const localPackageJson = require(paths.appPackageJson);
-    const { name, version } = localPackageJson;
+    const localPackageJson = await import(paths.appPackageJson);
+    const { name, version, scripts } = localPackageJson;
     const remoteVersion = await getRemoteVersion(name);
     const shouldPublish = gt(version, remoteVersion);
+
     console.log(
-      `local: ${version} remote: ${remoteVersion} publish: ${shouldPublish}`
+      `local: ${version} remote: ${remoteVersion} shouldPublish: ${shouldPublish}`
     );
 
     if (!shouldPublish) return console.log('Not Publishing');
 
-    console.log(`Building ${name} ...`);
-    await execShell('npm run build', { shouldLog: true });
+    if (scripts.build) {
+      console.log(`Building ${name} ...`);
+      await execShell('npm run build', { shouldLog: true });
+    }
+
     console.log(`Publishing ${name} ...`);
+    if (!process.env.NPM_AUTH_TOKEN) throw new Error('Midding NPM Auth Token');
     await execShell('npm publish', { shouldLog: true });
   } catch (error) {
     console.log('caught error', error);
-    throw error;
+    process.exit(1);
   }
 };
 
