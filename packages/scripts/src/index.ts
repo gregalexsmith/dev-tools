307c249d9c43abe9fd0c3a9905a22755c6a26998
greg smith <gregalexsmith@gmail.com>
2022-06-18 18:18:21 -0400

    chore(scripts): clean up

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,26 +1,6 @@
 #! /usr/bin/env node
-/* eslint-disable @typescript-eslint/no-var-requires */
 
 import { Command } from 'commander';
-// import minimist from 'minimist';
-
-// const main = async () => {
-//   const args = minimist(process.argv.slice(2));
-//   const cmd = args._[0];
-
-//   if (!cmd) return console.error('Please choose a command');
-
-//   switch (cmd) {
-//     case 'publish':
-//       break;
-//     default:
-//       console.error(`"${cmd}" is not a valid command!`);
-//       break;
-//   }
-// };
-
-// main();
-
 const program = new Command();
 
 program.name('development-scripts');
