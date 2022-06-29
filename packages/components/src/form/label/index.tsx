d2ef8488ad640d353ac56989fc6dbccfccb447c9
greg smith <gregalexsmith@gmail.com>
2022-06-28 22:46:50 -0400

    feat(components): add toggle

--- packages/components/src/form/label/index.tsx
+++ packages/components/src/form/label/index.tsx
@@ -3,14 +3,15 @@ import classNames from 'classnames';
 
 export type LabelProps = ComponentProps<'label'>;
 
-export const Label: FC<LabelProps> = ({ children, ...rest }) => {
+export const Label: FC<LabelProps> = ({ children, className, ...rest }) => {
   return (
     <label
       {...rest}
       className={classNames(
         'block mb-2',
         'text-sm font-medium',
-        'text-gray-900 dark:text-gray-300'
+        'text-gray-900 dark:text-gray-300',
+        className
       )}>
       {children}
     </label>
